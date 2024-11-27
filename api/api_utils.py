822cd94ebab1de410ebc17c4066c4615eaf04674
Alfonso Gambino <alfonso.gambino24@imperial.ac.uk>
2024-11-27 17:35:02 +0000

    api, defined fetch_book_detail() endpoint

---
+++ api/api_utils.py
@@ -0,0 +1,39 @@
+import requests
+
+def fetch_book_details(user_input):
+    # Validate input
+    user_input = user_input.strip()
+    if not user_input:
+        return {"error": "Search term is required"}
+    
+    # API url
+    base_url = "https://openlibrary.org/search.json"
+
+    # Make the API request
+    try:
+        response = requests.get(base_url, params={"q": user_input})
+        # Raises an error if the API is not responding
+        response.raise_for_status()
+    except requests.exeptions.RequestException as e:
+        return {"error": f"API request failed: {e}"}
+    
+    # Parse JSON response
+    data = response.json()
+
+    # Check for results
+    if not data.get("docs"):
+        return {"message": "No matching books found"}
+    
+    # Extract the first result
+    book = data["docs"][0]
+    cover_id = book.get("cover_i", None)
+    cover_image_url = f"https://covers.openlibrary.org/b/id/{cover_id}-L.jpg" if cover_id else None
+    title = book.get("title", "Unkown Title")
+    authors = book.get("author_name", ["Unkown Author"])
+
+    # Return book details
+    return {
+        "cover_image_url": cover_image_url,
+        "authors": authors,
+        "title": title
+    }
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
+from api.api_utils import fetch_book_details
 
 app = Flask(__name__)
-
-
+ 
 @app.route("/")
 def index():
     return render_template("index.html")
