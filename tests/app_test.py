d448c225bbbd4996a305240d1112d5cc9067124c
Zev Menachemson <zm1224@imperial.ac.uk>
2024-11-27 14:00:18 +0000

    Fixed test bugs and python formatting

--- tests/app_test.py
+++ tests/app_test.py
@@ -4,18 +4,19 @@
 from app import process_query
 
 
-def test_pi():
-    # Should fail
-    assert process_query("pi") == "i is an irrational number"
+def test_pi_lower():
+    assert process_query("pi") == "pi is an irrational number"
 
-def test_knows_lotr():
-    # Should pass
+def test_pi_upper():
+    assert process_query("PI") == "pi is an irrational number"
+
+def test_knows_lotr_author():
     assert process_query("Who is the author of LOTR") == "JRR Tolkein"
 
 
-api = 0
-expected_format = 0
+# api = 0
+# expected_format = 0
 
 
-def test_book_api():
-    assert process_query(api) == expected_format
+# def test_book_api():
+#     assert process_query(api) == expected_format
