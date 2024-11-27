e74939e35c8a37f658bded6008d96b9e1a3efe6b
Zev Menachemson <zm1224@imperial.ac.uk>
2024-11-27 12:53:52 +0000

    Added some default query tests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+def process_query(query):
+    if query.lower() == "pi":
+        return "pi is an irrational number"
+    elif query == "Who is the author of LOTR":
+        return "JRR Tolkein"
+    return "UNKNOWN"
+
+@app.route("/query", methods=["GET"]) # Do we need "POST" as well?
+def query():
+    return process_query(request.args.get('q'))
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,2 +1,16 @@
-def test_placholder():
-    assert 5 == 5
+# Tests to query the API
+
+from app import process_query
+
+def test_pi():
+    assert process_query("pi") == "i is an irrational number" # Should fail
+
+def test_knows_lotr():
+    assert process_query("Who is the author of LOTR") == "JRR Tolkein" # Should pass
+
+
+api = 0
+expected_format = 0
+def test_book_api():
+    assert process_query(api) == expected_format
+
