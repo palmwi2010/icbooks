ca74d5c1a042ca8b4f09cd40a7b0e968001bcc88
Will Palmer <wp624@ic.ac.uk>
2024-11-27 14:44:47 +0000

    built initial ui

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
+def get_book_url(book):
+    return r"https://covers.openlibrary.org/b/isbn/" + book['isbn'] + "-M.jpg"
 
-@app.route("/")
+def get_books(search_term = None):
+    with open('./static/books.json') as file:
+        books = json.load(file)["books"]
+        for bk in books:
+            bk['url'] = get_book_url(bk)
+        return books
+
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return render_template("index.html")
+    books = get_books()
+    return render_template("index.html", books = books)
+
+@app.route("/add", methods = ["GET", "POST"])
+def add():
+    return render_template("add.html")
