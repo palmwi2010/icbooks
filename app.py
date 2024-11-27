9be8d703a0131df94bdf049a7228cd678216f6dc
palmwi2010 <wop3000@hotmail.co.uk>
2024-11-27 21:25:34 +0000

    fixed formatting issues

--- app.py
+++ app.py
@@ -4,24 +4,29 @@ import json
 
 app = Flask(__name__)
 
+
 def get_book_url(book):
     return r"https://covers.openlibrary.org/b/isbn/" + book['isbn'] + "-M.jpg"
 
-def get_books(search_term = None):
+
+def get_books():
     with open('./static/books.json') as file:
         books = json.load(file)["books"]
         for bk in books:
             bk['url'] = get_book_url(bk)
         return books
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     books = get_books()
-    return render_template("index.html", books = books)
+    return render_template("index.html", books=books)
 
-@app.route("/add", methods = ["GET", "POST"])
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
     return render_template("add.html")
+
 
 def process_query(query):
     if query.lower() == "pi":
@@ -30,7 +35,7 @@ def process_query(query):
         return "JRR Tolkein"
     return "UNKNOWN"
 
-@app.route("/query", methods=["GET"]) # Do we need "POST" as well?
+
+@app.route("/query", methods=["GET"])  # Do we need "POST" as well?
 def query():
     return process_query(request.args.get('q'))
-  
--- tests/app_test.py
+++ tests/app_test.py
@@ -2,15 +2,19 @@
 
 from app import process_query
 
+
 def test_pi():
-    assert process_query("pi") == "i is an irrational number" # Should fail
+    assert process_query("pi") == "i is an irrational number"  # Should fail
+
 
 def test_knows_lotr():
-    assert process_query("Who is the author of LOTR") == "JRR Tolkein" # Should pass
+    query = "Who is the author of LOTR"
+    assert process_query(query) == "JRR Tolkein"  # Should pass
 
 
 api = 0
 expected_format = 0
+
+
 def test_book_api():
     assert process_query(api) == expected_format
-
