fe9b2d7f3a174599214666736f4cb4d54f57193b
palmwi2010 <wop3000@hotmail.co.uk>
2024-11-29 18:59:54 +0000

    made search ase insensitive

--- static/index.js
+++ static/index.js
@@ -8,9 +8,14 @@ $input.addEventListener("input", e => {
 const applyFilter = (search_term) => {
     const $cards = document.querySelectorAll(".card");
 
+    // case insensitive: take lower
+    const search_lower = search_term.toLowerCase();
+
     $cards.forEach($card => {
         const title = $card.querySelector(".title").innerText;
-        if (!title.startsWith(search_term)) {
+        const title_lower = title.toLowerCase(); // case insensitive
+
+        if (!title_lower.startsWith(search_lower)) {
             $card.style.display = "none";
         } else {
             $card.style.display = "flex";
